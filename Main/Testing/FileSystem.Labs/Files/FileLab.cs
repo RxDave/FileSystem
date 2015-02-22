872c0627ed33b582e3e30fbab5659722e32597a8
davedev <dave@davesexton.com>
2015-02-22 15:01:17 -0500

    - Added basic file and folder experiments to the labs. - Fixed the project references and updated the solution file.

---
+++ Main/Testing/FileSystem.Labs/Files/FileLab.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DaveSexton.Labs;
+
+namespace FileSystem.Labs.Files
+{
+  public sealed class FileLab : BaseConsoleLab
+  {
+    protected override void Main()
+    {
+      RunExperiments();
+    }
+
+    public async Task TemporaryFile()
+    {
+      var file = await FileSystemProvider.TemporaryStorage.CreateFileAsync("123.test");
+
+      await TraceFileAsync(file);
+
+      var contents = "Testing 123";
+
+      await WriteToFileAsync(file, contents);
+
+      TraceSuccess("Wrote: {0}", contents);
+
+      file = await FileSystemProvider.TemporaryStorage.GetFileAsync("123.test");
+
+      await TraceFileAsync(file);
+
+      contents = await ReadFromFileAsync(file);
+
+      TraceSuccess("Read: {0}", contents);
+
+      await file.DeleteAsync();
+
+      await TraceFileAsync(file);
+    }
+
+    public async Task LocalStorageFile()
+    {
+      var file = await FileSystemProvider.LocalStorage.CreateFileAsync("456.test");
+
+      await TraceFileAsync(file);
+
+      var contents = "Testing 456";
+
+      await WriteToFileAsync(file, contents);
+
+      TraceSuccess("Wrote: {0}", contents);
+
+      file = await FileSystemProvider.LocalStorage.GetFileAsync("456.test");
+
+      await TraceFileAsync(file);
+
+      contents = await ReadFromFileAsync(file);
+
+      TraceSuccess("Read: {0}", contents);
+
+      await file.DeleteAsync();
+
+      await TraceFileAsync(file);
+    }
+
+    public async Task MyDocumentsFile()
+    {
+      var folder = await FileSystemProvider.GetFolderAsync(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+      await FolderLab.TraceFolderAsync(folder);
+
+      var file = await folder.CreateFileAsync("789.test");
+
+      await TraceFileAsync(file);
+
+      var contents = "Testing 789";
+
+      await WriteToFileAsync(file, contents);
+
+      TraceSuccess("Wrote: {0}", contents);
+
+      file = await folder.GetFileAsync("789.test");
+
+      await TraceFileAsync(file);
+
+      contents = await ReadFromFileAsync(file);
+
+      TraceSuccess("Read: {0}", contents);
+
+      await file.DeleteAsync();
+
+      await TraceFileAsync(file);
+    }
+
+    private static async Task WriteToFileAsync(IFile file, string contents)
+    {
+      using (var stream = await file.OpenAsync(forWriting: true))
+      {
+        var bytes = Encoding.UTF8.GetBytes(contents);
+
+        await stream.WriteAsync(bytes, 0, bytes.Length);
+      }
+    }
+
+    private static async Task<string> ReadFromFileAsync(IFile file)
+    {
+      using (var stream = await file.OpenSequentialReadAsync())
+      using (var reader = new StreamReader(stream))
+      {
+        return await reader.ReadToEndAsync();
+      }
+    }
+
+    public static async Task TraceFileAsync(IFile file)
+    {
+      if (await file.ExistsAsync())
+      {
+        LabTraceSource.Default.TraceSuccess(file.FullPath);
+      }
+      else
+      {
+        LabTraceSource.Default.TraceFailure(file.FullPath);
+      }
+    }
+  }
+}
---
+++ Main/Testing/FileSystem.Labs/Files/FolderLab.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+using DaveSexton.Labs;
+
+namespace FileSystem.Labs.Files
+{
+  public sealed class FolderLab : BaseConsoleLab
+  {
+    protected override void Main()
+    {
+      RunExperiments();
+    }
+
+    public async Task TemporaryFolder()
+    {
+      var folder = await FileSystemProvider.TemporaryStorage.CreateFolderAsync("123");
+
+      await TraceFolderAsync(folder);
+
+      folder = await FileSystemProvider.TemporaryStorage.GetFolderAsync("123");
+
+      await TraceFolderAsync(folder);
+
+      await folder.DeleteAsync();
+
+      await TraceFolderAsync(folder);
+    }
+
+    public async Task LocalStorageFolder()
+    {
+      var folder = await FileSystemProvider.LocalStorage.CreateFolderAsync("456");
+
+      await TraceFolderAsync(folder);
+
+      folder = await FileSystemProvider.LocalStorage.GetFolderAsync("456");
+
+      await TraceFolderAsync(folder);
+
+      await folder.DeleteAsync();
+
+      await TraceFolderAsync(folder);
+    }
+
+    public async Task MyDocumentsFolder()
+    {
+      var documents = await FileSystemProvider.GetFolderAsync(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+      await TraceFolderAsync(documents);
+
+      var folder = await documents.CreateFolderAsync("789");
+
+      await TraceFolderAsync(folder);
+
+      folder = await documents.GetFolderAsync("789");
+
+      await TraceFolderAsync(folder);
+
+      await folder.DeleteAsync();
+
+      await TraceFolderAsync(folder);
+    }
+
+    public static async Task TraceFolderAsync(IFolder folder)
+    {
+      if (await folder.ExistsAsync())
+      {
+        LabTraceSource.Default.TraceSuccess(folder.FullPath);
+      }
+      else
+      {
+        LabTraceSource.Default.TraceFailure(folder.FullPath);
+      }
+    }
+  }
+}
